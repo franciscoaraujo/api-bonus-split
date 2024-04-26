ba7bab3dab542e14c899a47d85bf27c0e7a39edf
Francisco Araujo <francisco.araujo@redeflex.com.br>
2024-04-26 17:39:55 -0300

    first commit

---
+++ src/main/java/com/bytebuilder/apibonussplit/ApiBonusSplitApplication.java
@@ -0,0 +1,13 @@
+package com.bytebuilder.apibonussplit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBonusSplitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiBonusSplitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bytebuilder/apibonussplit/ApiBonusSplitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bytebuilder.apibonussplit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBonusSplitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
